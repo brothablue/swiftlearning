2c18d2455a303a334c96f901d237d3acae1ffae0
Me <BrothaBlue@dannys-macbook-pro.local>
2018-10-01 17:24:15 -0400

    Initial Commit

---
+++ Swift Learning/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift Learning
+//
+//  Created by Danny Blue on 10/1/18.
+//  Copyright © 2018 Danny Blue. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
